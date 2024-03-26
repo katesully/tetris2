7f7f0b0fd61837e28de9bbb9e07f98ec1ce0f9d8
katesully <30308611+katesully@users.noreply.github.com>
2024-03-26 10:12:42 -0700

    Add files via upload
    Initial commit for working file

---
+++ game.py
@@ -0,0 +1,93 @@
+import random
+import pygame
+from command import Command
+from grid import Grid
+from resources.ui import UI
+from tetrominos import (
+    ITetromino,
+    OTetromino,
+    ZTetromino,
+    TTetromino,
+    STetromino,
+    JTetromino,
+    LTetromino,
+)
+
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode(
+            (500, 620)
+        )  # set the screen size to 300x600 . 20rowsx10columns grid
+        self.game_over = False
+        self.score = 0
+
+        # game objects
+        self.grid = Grid()
+        self.tetromino = self.spawn_new_tetromino()
+        self.ui = UI()
+        self.next_tetromino = self.spawn_new_tetromino()
+
+    def run(self):
+
+        event_every_200ms = pygame.USEREVENT + 1
+        pygame.time.set_timer(event_every_200ms, 200)
+
+        while True:
+            command = None
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE and self.game_over == True:
+                        self.__init__()
+                    if event.key == pygame.K_LEFT and self.game_over == False:
+                        command = Command.LEFT
+                    if event.key == pygame.K_RIGHT and self.game_over == False:
+                        command = Command.RIGHT
+                    if event.key == pygame.K_DOWN and self.game_over == False:
+                        command = Command.DOWN
+                        self.update_score(0, 1)
+                    if event.key == pygame.K_UP and self.game_over == False:
+                        command = Command.UP
+                elif event.type == event_every_200ms and self.game_over == False:
+                    command = Command.DOWN
+
+            self.update(command)
+            self.draw()
+            pygame.display.update()
+
+    def draw(self):
+        self.ui.draw(self.screen, self)
+        self.grid.draw(self.screen, 10, 10)
+        self.tetromino.draw(self.screen, 10, 10)
+
+    def update(self, command):
+        self.tetromino.update(command, self)
+
+    def spawn_new_tetromino(self):
+        return random.choice(
+            [
+                TTetromino(),
+                ITetromino(),
+                OTetromino(),
+                ZTetromino(),
+                STetromino(),
+                JTetromino(),
+                LTetromino(),
+            ]
+        )
+
+    def check_for_any_full_lines_to_clear(self):
+        return self.grid.check_for_any_full_lines_to_clear()
+
+    def update_score(self, lined_cleared, move_down_points):
+        self.score += move_down_points
+        if lined_cleared == 1:
+            self.score += 100
+        if lined_cleared == 2:
+            self.score += 300
+        if lined_cleared == 3:
+            self.score += 500
---
+++ tetrominos.py
@@ -0,0 +1,287 @@
+import pygame
+from command import Command
+from resources.colors import Colors
+
+
+class Tetromino:
+    def __init__(self) -> None:
+        self.state = 0
+        self.col_offset = 4
+        self.row_offset = 0
+
+    def draw(self, screen, ui_x_offset, ui_y_offset):
+        for row_index, row in enumerate(self.blocks[self.state]):
+            for col_index, block in enumerate(row):
+                if block:
+                    pygame.draw.rect(
+                        screen,
+                        self.color,
+                        (
+                            (col_index + self.col_offset) * 30 + ui_x_offset,
+                            (row_index + self.row_offset) * 30 + ui_y_offset,
+                            30 - 1,
+                            30 - 1,
+                        ),
+                    )
+
+    def update(self, command: Command, game):
+        if command == Command.LEFT:
+            self.move_left(game)
+        if command == Command.RIGHT:
+            self.move_right(game)
+        if command == Command.DOWN:
+            self.move_down(game)
+        if command == Command.UP:
+            self.rotate()
+
+    def move_left(self, game):
+        self.col_offset -= 1
+        if self.out_of_bounds():
+            self.col_offset += 1
+
+        if self.collides_with_other_tetrominos(game):
+            self.col_offset += 1
+
+    def move_right(self, game):
+        self.col_offset += 1
+        if self.out_of_bounds():
+            self.col_offset -= 1
+
+        if self.collides_with_other_tetrominos(game):
+            self.col_offset -= 1
+
+    def move_down(self, game):
+        self.row_offset += 1
+
+        if self.collides_with_other_tetrominos(game):
+            self.row_offset -= 1
+            if self.row_offset <= 0:
+                print("game over")
+                game.game_over = True
+                return
+            self.lock_tetromino(game)
+            game.tetromino = game.next_tetromino
+            game.next_tetromino = game.spawn_new_tetromino()
+            return
+
+        if self.out_of_bounds():
+            self.row_offset -= 1
+            self.lock_tetromino(game)
+            game.tetromino = game.next_tetromino
+            game.next_tetromino = game.spawn_new_tetromino()
+            return
+
+    def out_of_bounds(self):
+        for row_index, row in enumerate(self.blocks[self.state]):
+            for col_index, block in enumerate(row):
+                if block:
+                    if (
+                        col_index + self.col_offset < 0
+                        or col_index + self.col_offset > 9
+                        or row_index + self.row_offset > 19
+                    ):
+                        return True
+        return False
+
+    def rotate(self):
+        self.state = (self.state + 1) % len(self.blocks)
+        if self.out_of_bounds():
+            self.state = (self.state - 1) % len(self.blocks)
+
+    def lock_tetromino(self, game):
+        """
+        Carbon copy the tetromino into the grid
+        """
+        for row_index, row in enumerate(self.blocks[self.state]):
+            for col_index, block in enumerate(row):
+                if block:
+                    game.grid.blocks[row_index + self.row_offset][
+                        col_index + self.col_offset
+                    ] = self.color
+        lined_cleared = game.check_for_any_full_lines_to_clear()
+        game.update_score(lined_cleared, 0)
+
+    def collides_with_other_tetrominos(self, game):
+        for row_index, row in enumerate(self.blocks[self.state]):
+            for col_index, block in enumerate(row):
+                if block:
+                    if row_index + self.row_offset <= 19:
+                        if game.grid.blocks[row_index + self.row_offset][
+                            col_index + self.col_offset
+                        ]:
+                            return True
+        return False
+
+
+class ZTetromino(Tetromino):
+    def __init__(self) -> None:
+        super().__init__()
+        self.color = Colors.BLUE.value
+        self.blocks = [
+            [
+                [1, 1, 0],
+                [0, 1, 1],
+                [0, 0, 0],
+            ],
+            [
+                [0, 0, 1],
+                [0, 1, 1],
+                [0, 1, 0],
+            ],
+            [
+                [0, 0, 0],
+                [1, 1, 0],
+                [0, 1, 1],
+            ],
+            [
+                [0, 1, 0],
+                [1, 1, 0],
+                [1, 0, 0],
+            ],
+        ]
+
+
+class OTetromino(Tetromino):
+    def __init__(self) -> None:
+        super().__init__()
+        self.color = Colors.YELLOW.value
+        self.blocks = [
+            [
+                [1, 1],
+                [1, 1],
+            ]
+        ]
+
+
+class ITetromino(Tetromino):
+    def __init__(self) -> None:
+        super().__init__()
+        self.color = Colors.ORANGE.value
+        self.blocks = [
+            [
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+            ],
+            [
+                [0, 0, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 1, 0],
+            ],
+        ]
+        self.col_offset = 3
+        self.row_offset = -1
+
+
+class STetromino(Tetromino):
+    def __init__(self) -> None:
+        super().__init__()
+        self.color = Colors.PURPLE.value
+        self.blocks = [
+            [
+                [0, 1, 1],
+                [1, 1, 0],
+                [0, 0, 0],
+            ],
+            [
+                [0, 1, 0],
+                [0, 1, 1],
+                [0, 0, 1],
+            ],
+            [
+                [0, 0, 0],
+                [0, 1, 1],
+                [1, 1, 0],
+            ],
+            [
+                [1, 0, 0],
+                [1, 1, 0],
+                [0, 1, 0],
+            ],
+        ]
+
+
+class JTetromino(Tetromino):
+    def __init__(self) -> None:
+        super().__init__()
+        self.color = Colors.RED.value
+        self.blocks = [
+            [
+                [1, 0, 0],
+                [1, 1, 1],
+                [0, 0, 0],
+            ],
+            [
+                [0, 1, 1],
+                [0, 1, 0],
+                [0, 1, 0],
+            ],
+            [
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 0, 1],
+            ],
+            [
+                [0, 1, 0],
+                [0, 1, 0],
+                [1, 1, 0],
+            ],
+        ]
+
+
+class LTetromino(Tetromino):
+    def __init__(self) -> None:
+        super().__init__()
+        self.color = Colors.GREEN.value
+        self.blocks = [
+            [
+                [0, 0, 1],
+                [1, 1, 1],
+                [0, 0, 0],
+            ],
+            [
+                [0, 1, 0],
+                [0, 1, 0],
+                [0, 1, 1],
+            ],
+            [
+                [0, 0, 0],
+                [1, 1, 1],
+                [1, 0, 0],
+            ],
+            [
+                [1, 1, 0],
+                [0, 1, 0],
+                [0, 1, 0],
+            ],
+        ]
+
+
+class TTetromino(Tetromino):
+    def __init__(self) -> None:
+        super().__init__()
+        self.color = Colors.CYAN.value
+        self.blocks = [
+            [
+                [0, 1, 0],
+                [1, 1, 1],
+                [0, 0, 0],
+            ],
+            [
+                [0, 1, 0],
+                [0, 1, 1],
+                [0, 1, 0],
+            ],
+            [
+                [0, 0, 0],
+                [1, 1, 1],
+                [0, 1, 0],
+            ],
+            [
+                [0, 1, 0],
+                [1, 1, 0],
+                [0, 1, 0],
+            ],
+        ]
